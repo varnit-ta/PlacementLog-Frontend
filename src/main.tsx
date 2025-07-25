ce7efa6d9d292db5e887c9ebbc84c3010bdb079d
varnit-ta <varnitsingh65@gmail.com>
2025-07-26 01:32:30 +0530

    fixed SpeedInsight error

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import { PostsContextProvider } from "./context/posts-context.tsx";
 import { UserContextProvider } from "./context/user-context.tsx";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
